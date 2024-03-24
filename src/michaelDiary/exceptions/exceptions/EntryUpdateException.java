ef70a7e69a0bd1fa43111662eac02cf7f5c30034
michael <dikandumichael@gmail.com>
2024-03-24 15:50:01 +0100

    Spring boot first project

--- src/michaelDiary/exceptions/exceptions/EntryUpdateException.java
+++ src/michaelDiary/exceptions/exceptions/EntryUpdateException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package michaelDiary.exceptions.exceptions;
 
 public class EntryUpdateException extends RuntimeException{
     public EntryUpdateException(String message){super(message);}
