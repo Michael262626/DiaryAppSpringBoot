38464877c855fbfc8a484794e083535f3537e6fa
michael <dikandumichael@gmail.com>
2024-03-24 15:48:22 +0100

    Spring boot first project

--- src/michaelDiary/exceptions/exceptions/UserNotFoundException.java
+++ src/michaelDiary/exceptions/exceptions/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package michaelDiary.exceptions.exceptions;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String message){super(message);}
