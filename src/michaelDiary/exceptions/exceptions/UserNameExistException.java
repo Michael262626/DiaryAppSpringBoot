bd44c075c6fd60936f9f6f3bccf6dbcb01bc0d82
michael <dikandumichael@gmail.com>
2024-03-24 15:48:33 +0100

    Spring boot first project

--- src/michaelDiary/exceptions/exceptions/UserNameExistException.java
+++ src/michaelDiary/exceptions/exceptions/UserNameExistException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package michaelDiary.exceptions.exceptions;
 
 public class UserNameExistException extends RuntimeException{
     public UserNameExistException(String message){super(message);}
